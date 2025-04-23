45a597a080dbc63fa57d46f791974dcd5ae33372
SantiagoArce0606 <fearmylove38@outlook.com>
2025-04-22 20:53:45 -0600

    Update Post
    Crud del foro y boton volcer al foro

--- functions/Post.php
+++ functions/Post.php
@@ -1,8 +1,37 @@
 <?php 
+    
+    require_once('conexion.php');
+    
     require_once('../includes/header.php');
     require_once('../includes/navbar.php');
 
     $error = ""; 
+    $succes = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        if (!isset($_SESSION['usuario'])){
+            $error = 'Debes de iniciar sesion para poder publicar';
+        } else {
+            $usuario_id = $_SESSION['usuario']['USUARIOS_ID'];
+            $titulo = $_POST['Titulo'] ?? '';
+            $descripcion = $_POST['Descripcion'] ?? '';
+            $link = $_POST['Link'] ?? '';
+            $fecha = date ('Y-m-d');
+
+            if(empty($titulo) || empty($descripcion) || empty($link)){
+                $error = 'Porfavor llenar todos los espacios';
+            } else {
+            $stmt = $conexion->prepare ("INSERT INTO foro (USUARIO_ID, LINK, FECHA_PUBLICACION, DESCRIPCION) VALUES (?, ?, ?, ?)");
+            $stmt->bind_param ("isss", $usuario_id, $link, $fecha, $descripcion);
+
+            if ($stmt-> execute()){
+                $mensaje = 'Publicacion Exitosa';
+            } else {
+                $error = 'Fallo al guardar la publicacion: '. $conexion -> error;
+            }
+            }
+        }
+    }
     ?> 
 
 <div class="container mt-5">
@@ -17,7 +46,7 @@
                         <div class="alert alert-danger"><?php echo $error; ?></div>
                     <?php endif; ?>
                     
-                    <form method="POSTP" action="register.php">
+                    <form method="POST" action="Post.php">
                         <div class="mb-3">
                             <label for="Titulo" class="form-label">Titulo</label>
                             <input type="text" class="form-control" id="Titulo" name="Titulo" required>
@@ -30,10 +59,9 @@
                             <label for="Link" class="form-label">Link</label>
                             <input type="text" class="form-control" id="Link" name="Link" required>
                         </div>
-                        <button type="submit" class="btn btn-success w-100">Publicar</button>
-                    </form>
-                    <div class="mt-3 text-center">
-                        <p>¿Ya hiciste una publicacion? <a href="../foro.php">Vuelve al foro</a></p>
+                        <div class="d-flex justify-content-between">
+                       <button type="submit" class="btn btn-success">Publicar</button>
+                       <a href="../foro.php" class="btn btn-success">Volver al foro</a>
                     </div>
                 </div>
             </div>
