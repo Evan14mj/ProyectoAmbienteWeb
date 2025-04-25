de46d9856a0cdc3c28a7629a3818c2e317c9bc1c
SantiagoArce0606 <fearmylove38@outlook.com>
2025-04-24 19:18:46 -0600

    Interfaz del foro
    La interfaz del foro esta hecha con la card básica de bootstrap y el botón de publicar también es un button básico de bootstrap

--- foro.php
+++ foro.php
@@ -15,4 +15,33 @@
                   ORDER BY f.FECHA_PUBLICACION DESC";
         $result = $conexion->query($query);
 
+        if ($result && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+
             ?>
+         <div class="col-md-4 mb-4 d-flex justify-content-center">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo ($row['TITULO']); ?></h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">Por: <?php echo ($row['NOMBRE']); ?></h6>
+                    <p class="card-text"><?php echo ($row['DESCRIPCION']); ?></p>
+                    <a href="<?php echo ($row['LINK']); ?>" class="card-link" target="_blank">Ir al tema</a>
+                    <span class="card-link text-muted"><?php echo date('d/m/Y', strtotime($row['FECHA_PUBLICACION'])); ?></span>
+                </div>
+            </div>
+        </div>
+        <?php 
+            }
+        } else {
+            echo "<p class='text-center'>No hay publicaciones disponibles en el foro.</p>";
+        }
+        ?>
+    </div>
+</div>
+
+<div class="d-flex justify-content-center mb-4">
+        <a href="functions/Post.php">
+            <button type="button" class="btn btn-success">Agregar Post</button>
+        </a>
+    </div>
+    
